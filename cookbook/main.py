1fa83da1c2cf75ddd9afb4d14caa6ef12a59d651
isayhc <isayahculbertson@gmail.com>
2024-06-03 18:57:44 -0400

    allow for german ocr

--- cookbook/main.py
+++ cookbook/main.py
@@ -1,10 +1,12 @@
 # https://tesseract-ocr.github.io/tessdoc/Installation.html
 # https://pypi.org/project/pytesseract/
 
+# sudo apt install tesseract-ocr-deu
+
 from PIL import Image
 import pytesseract
 
 if __name__ == "__main__":
     file_location = "assets/flow-chart.png"
 
-    print(pytesseract.image_to_data(Image.open(file_location)))
+    print(pytesseract.image_to_data(Image.open(file_location),lang="deu")))
