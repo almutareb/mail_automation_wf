6f3a4f2e7ff8eb60c225b11ceabd83fdb62c98ce
isayhc <isayahculbertson@gmail.com>
2024-06-03 19:00:25 -0400

    removed unpaired paranthesis

--- cookbook/main.py
+++ cookbook/main.py
@@ -9,4 +9,4 @@ import pytesseract
 if __name__ == "__main__":
     file_location = "assets/flow-chart.png"
 
-    print(pytesseract.image_to_data(Image.open(file_location),lang="deu")))
+    print(pytesseract.image_to_data(Image.open(file_location),lang="deu"))
