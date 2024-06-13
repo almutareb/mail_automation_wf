507f927b49b78626e5f6572ef98add676539c2c4
isayhc <isayahculbertson@gmail.com>
2024-06-12 20:11:32 -0400

    added typing and refactored code

--- mail_automation_wf/document_parser/document_parser.py
+++ mail_automation_wf/document_parser/document_parser.py
@@ -1,10 +1,31 @@
 import os
 from pathlib import Path
 from pdf2image import convert_from_path
-from typing import List, Dict
+from typing import List, Dict, Any
 import pytesseract
 from pytesseract import Output
 from PIL import Image
+
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+import json
+# from mail_automation_wf.utils.file_handling import  json_string_to_json
+
+def json_string_to_json(json_string: str) -> Any:
+    """
+    Converts a JSON string into a JSON object.
+
+    Args:
+        json_string (str): The JSON string to be converted.
+
+    Returns:
+        Any: The resulting JSON object (usually a dict or list).
+    """
+    try:
+        return json.loads(json_string)
+    except json.JSONDecodeError as e:
+        print(f"An error occurred while parsing the JSON string: {e}")
+        return None
 
 class DocumentParser:
     """
@@ -81,13 +102,47 @@ class DocumentParser:
     
 if __name__ == "__main__":
     
-    dd = DocumentParser(
+    document_parser = DocumentParser(
         pdf_file_location="examples/blank_documenr.pdf",
         ocr_output_location="test_sample",
     )
     
     
-    dd.pdf_to_images()
+    document_parser.pdf_to_images()
     
-    data = dd.image_to_text()
+    data = document_parser.image_to_text()
+    
+    # get the text from all pages
+    all_valid_text = [i['text'] for i in data]
+    
+    new_all_valid_text = []
+    
+    for text in all_valid_text:
+        filter_text =  [i for i in text if i.strip()]
+        new_all_valid_text.extend(filter_text)
+    
+    template = """ please attempt to organize the {unstrucutred_data} from the OCR 
+    and try to properly group it and turn it into a json format.  
+    """
+
+    prompt = PromptTemplate.from_template(template)
+
+
+    llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.3", 
+    temperature=0.1, 
+    max_new_tokens=1024,
+    repetition_penalty=1.2,
+    return_full_text=False
+        )
+
+
+    llm_chain = prompt | llm
+
+    
+    temp_data_name:List[str] = [llm_chain.invoke({"unstrucutred_data": i}) for i in new_all_valid_text]
+
+    # Will return None if the LLM did not format the output correctly
+    json_ouput_as_json:List[Dict] = [json_string_to_json(i) for i in temp_data_name]
+
     x = 0
