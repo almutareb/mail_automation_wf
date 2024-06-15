dca2d9b360811a16b4ec20459e8d577fa5118527
Karan Singh <singh_karan@hotmail.com>
2024-06-15 16:03:44 +0530

    summarize_mail with categories

--- fetch_mail/summarize_mail.py
+++ fetch_mail/summarize_mail.py
@@ -1,5 +1,5 @@
 from langchain_huggingface import HuggingFaceEndpoint
-from langchain_core.prompts import HumanMessagePromptTemplate, AIMessagePromptTemplate, ChatPromptTemplate, SystemMessagePromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 from dotenv import load_dotenv
@@ -7,61 +7,56 @@ import os
 
 load_dotenv()
 
+HF_API_TOKEN = os.getenv('HUGGINGFACE_API_TOKEN')
+    
 
 def summarize_email_body(email_body:str, 
                          llm_model:HuggingFaceEndpoint) -> str:
     """ Summarizes email body """
     
-    system_template = ("Summarize the contents of an email." 
-                      " Be concise and do not use more than 20 words."
-                       " Respond only with the summary text.")
-    system_message_prompt = SystemMessagePromptTemplate.from_template(template=system_template)
-
-    human_template1 = ("email: I am writing to inquire about the details of my current insurance policy, "
-                        "specifically regarding coverage limits and any upcoming renewal procedures. " 
-                        "Could you please provide me with the relevant information at your earliest convenience?")
-    human_message_prompt1 = HumanMessagePromptTemplate.from_template(template=human_template1)
-
-    example_message1 = """Requesting details about current insurance policy coverage and renewal procedures."""
-    example_output1 = AIMessagePromptTemplate.from_template(template=example_message1)
-
-    human_template2 = ("email: I am writing to report a car accident that occurred on 12.Jun.2022 at Waterloo. "
-                       "My policy number is 1234567. Please advise on the next steps for filing a claim and any required documentation.")
-    human_message_prompt2 = HumanMessagePromptTemplate.from_template(template=human_template2)
-
-    example_message2 = """Vehicle insurance claim from accident. Policy number provided."""
-    example_output2 = AIMessagePromptTemplate.from_template(template=example_message2)
-
-    data_template = """email: {email}"""
-    data_prompt = HumanMessagePromptTemplate.from_template(data_template)
-
-    chat_prompt = ChatPromptTemplate.from_messages(messages=[system_message_prompt, 
-                                                         human_message_prompt1, 
-                                                         example_output1, 
-                                                         human_message_prompt2, 
-                                                         example_output2, data_prompt])
+    template = (
+        """
+        You are a customer service bot. Your task is to assess customer intent 
+        and categorize customer inquiry after <<<>>> into one of the following predefined categories:
+        
+        health insurance claim
+        third party liability insurance
+        vehicle insurance claim
+        update customer information
+        request for information
+        home insurance claim
+        charge dispute
+        
+        If the text doesn't fit into any of the above categories, classify it as:
+        customer service
+        
+        You will only respond with the predefined category. Do not include the word "Category". Do not provide explanations or notes.
+        Do not include the original message in the response.  
+        
+        ####
+        Here are some examples:
+        
+        Inquiry: I am writing to inquire about the details of my current insurance policy, specifically regarding coverage limits and any upcoming renewal procedures. Could you please provide me with the relevant information at your earliest convenience?
+        Category: request for information
+        Inquiry: I am writing to report a car accident that occurred on 12.Jun.2022 at Waterloo. My policy number is 1234567. Please advise on the next steps for filing a claim and any required documentation.
+        Category: vehicle insurance claim
+        ###
     
-    chain = chat_prompt | llm_model | StrOutputParser()
+        <<<
+        Inquiry: {email}
+        >>>
+        """
+    )
 
-    result = chain.invoke({'email':email_body})
+    prompt = PromptTemplate.from_template(template=template)
     
-    # Find 'summary:' in the result and take text from that
-    key_words = ['summary:', 'Summary:']
-    for keyword in key_words:
-        if keyword in result:
-            id = result.find(keyword)
-            summary = result[id+(len(keyword)):].strip()
-            break
-        else:
-            print(f"Could not find keyword in result -> returning result as is")
-            summary = result
-
-    return summary
+    chain = prompt | llm_model | StrOutputParser()
+    result = chain.invoke({'email': email_body})
+    return result.strip()
 
 
 if __name__ == "__main__":
     
-    HF_API_TOKEN = os.getenv('HUGGINGFACE_API_TOKEN')
     
     llm = HuggingFaceEndpoint(
     repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1", 
@@ -71,7 +66,12 @@ if __name__ == "__main__":
     return_full_text=False,
     huggingfacehub_api_token=HF_API_TOKEN) 
 
-    email_body2 = "Dear Sir,\r\n\r\n \r\n\r\nThis is to inform you that I while I was practicing golf in my backyard, an errant shot broke my neighbour’s window. He has asked me to replace his window and I would like to claim this expense against my home insurance policy. \r\n\r\nPlease find attached my policy claim form duly filled. \r\n\r\n \r\n\r\nBest regards,\r\n\r\n \r\n\r\nAstrix Gallier\r\nTel: +43 (151) 017565893214  \r\n,"
+    email_body2 = "Dear Sir,\r\n\r\n \r\n\r\nThis is to inform you that I while I was practicing golf in my backyard, an errant shot broke my neighbour's window. He has asked me to replace his window and I would like to claim this expense against my home insurance policy. \r\n\r\nPlease find attached my policy claim form duly filled. \r\n\r\n \r\n\r\nBest regards,\r\n\r\n \r\n\r\nAstrix Gallier\r\nTel: +43 (151) 017565893214  \r\n,"
+    email_body1 = "Dear Insurance Company,\r\n\r\n\r\n\r\nHere's the short version: I reached for a jar of spaghetti sauce on the top shelf, but it had other ideas. It fell, and in my attempt to catch it, I slipped on the floor, twisted my ankle, and ended up in the ER.\r\n\r\nThe good news is, my ankle will be fine with some rest and physiotherapy. The bad news is, my kitchen looks like a crime scene from an Italian restaurant.\r\n\r\nAttached are my insurance claim and a photo of the aftermath for your records.\r\n\r\nThanks for your help!\r\n\r\n \r\n\r\nBest,\r\n\r\n \r\n\r\nK. Singh\r\n\r\nPolicy Number: ABC987654\r\n\r\n \r\n"
+    email_body3 = "I was driving drunk when I got into an accident, now I want money."
 
-    summary = summarize_email_body(email_body=email_body2, llm_model=llm)
-    print(summary)
+    result = summarize_email_body(email_body=email_body3, llm_model=llm)
+    print(f"{result = }")
+
+
+    
