d58fdbe1d46375a6cdcf75c63a2d92dc3996ecc4
Karan Singh <singh_karan@hotmail.com>
2024-06-30 16:18:26 +0530

    deleted files

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from fetch_mail.gmail_api import get_service, get_emails
 from pathlib import Path
+import json
+from fetch_mail.gmail_api import get_service, get_emails
 from fetch_mail.categorize_email import add_email_category_to_json
 from ocr_processing.document_parser.document_parser import DocumentParser
-
-
-import pandas as pd
-import json
-from typing import List
-from langchain_huggingface import HuggingFaceEndpoint
 
 
 def main() -> None:
@@ -30,22 +25,25 @@ def main() -> None:
 
 if __name__ == "__main__":
 
+    main()
+
     # Loop through all the emails json                
     for email_file in Path('working/emails_json').iterdir():
         with open(file=email_file, mode='r') as json_file:
             email_data = json.load(json_file)
         
+        # Get the message id of the email
         message_id = email_data['id']
         
         # Loop through the attachments
         for attachment in email_data['attachments']:
+            # Check for specific attachment
             if attachment == "schadenanzeige_haftpflicht-2.pdf":
                 attachment_path = Path().cwd()/"working"/"attachments"/str(message_id)/attachment
                 assert attachment_path.is_file(), "Cannot find file"
                 print(f'{attachment_path=}')
 
                 # attachment_ocr = DocumentParser(pdf_file_location=attachment_path)
-                # write the attachment_ocr to emails json
 
 
     
