a06659fe80042ff34ae5f3eca636ed49061e1fca
Asaad Almutareb <asaad@mutareb.com>
2024-06-30 19:23:25 +0000

    Merge pull request #37 from almutareb/30-move-ocr-processing-module-into-its-own-folder-1
    moved the example code snippets into /cookbook

--- cookbook/document_parser_example.py
+++ cookbook/document_parser_example.py
@@ -1,3 +1,4 @@
+<<<<<<<< HEAD:cookbook/document_parser_example.py
 from ocr_processing.document_parser.document_parser import DocumentParser
 from ocr_processing.utils.file_handling import  (
     extract_json_from_string, json_string_to_json
@@ -12,79 +13,54 @@ from ocr_processing.chains import (
     document_qa_prompt,
     user_questions_prompt
     )
+========
+from pathlib import Path
+import json
+from fetch_mail.gmail_api import get_service, get_emails
+from fetch_mail.categorize_email import add_email_category_to_json
+from ocr_processing.document_parser.document_parser import DocumentParser
+
+
+def main() -> None:
+
+    # Authenticate Gmail 
+    service = get_service(credentials_json='fetch_mail/credentials.json', token_file='fetch_mail/token.pickle')
+
+    # Get emails
+    message_ids = get_emails(service=service,
+                             subject_filter = "Test_App_Email_1234",
+                             attachment_dir = 'working/attachments',
+                             emails_json_dir = 'working/emails_json',
+                             status_file = 'working/email_status.csv')
+    
+    # Go through all the emails and add categories -> does not check if already categorized
+    for email_file in Path('working/emails_json').iterdir():
+        add_email_category_to_json(email_json_file=email_file, status_file='working/email_status.csv')
+        print('Categorizing email')
+
+>>>>>>>> b099545 (Merge pull request #37 from almutareb/30-move-ocr-processing-module-into-its-own-folder-1):main.py
 
 if __name__ == "__main__":
-    document_parser = DocumentParser(
-        # pdf_file_location="examples/blank_documenr.pdf",
-        pdf_file_location="examples/populated_document.pdf",
-        ocr_output_location="test_sample",
-    )
-    
-    document_parser.pdf_to_images()
-    
-    data = document_parser.image_to_text()
-    
-    ocr_document_text:List[str] = [i.text for i in document_parser.ocr_documents]
-    
 
-    llm = HuggingFaceEndpoint(
-    repo_id="mistralai/Mistral-7B-Instruct-v0.3", 
-    max_new_tokens=1024,
-    # max_new_tokens=2024,
-    repetition_penalty=1.2,
-    return_full_text=False
-        )
-    
+    main()
 
-    sample_text = ocr_document_text [0]
-    
-    mid_index = len(sample_text) // 2
-
-    sample_text = sample_text[:mid_index], sample_text[mid_index:]
-
-    ocr_structure_chain = ocr_structure_prompt | llm
-    temp_data_name:List[str] = [ocr_structure_chain.invoke({"unstrucutred_data": i}) for i in sample_text]
-    
-    try: 
-        ocr_json = extract_json_from_string(temp_data_name[0])
-        print("pre-processing document was successful")
-    except:
-        print("Issue pre parsing the json")
+    # Loop through all the emails json                
+    for email_file in Path('working/emails_json').iterdir():
+        with open(file=email_file, mode='r') as json_file:
+            email_data = json.load(json_file)
         
-    try:
-        ocr_json = json_string_to_json(temp_data_name[0])
-        print("Sucessful without preproccessing")
-    except:
-        print("parsing unsuccessfull")
+        # Get the message id of the email
+        message_id = email_data['id']
         
+        # Loop through the attachments
+        for attachment in email_data['attachments']:
+            # Check for specific attachment
+            if attachment == "schadenanzeige_haftpflicht-2.pdf":
+                attachment_path = Path().cwd()/"working"/"attachments"/str(message_id)/attachment
+                assert attachment_path.is_file(), "Cannot find file"
+                print(f'{attachment_path=}')
+
+                # attachment_ocr = DocumentParser(pdf_file_location=attachment_path)
+
+
     
-    summary_chain = json_summary_chain_prompt | llm
-    
-    document_summary = summary_chain.invoke({"unstrucutred_data": str(ocr_json)})
-    
-    
-    classify_document_chain = document_classification_chain_prompt | llm 
-    
-    document_type = classify_document_chain.invoke({"document": str(ocr_json)})
-    
-    document_qa_chain = document_qa_prompt | llm
-    
-    qa_answers = document_qa_chain.invoke({"JSONOCR": str(ocr_json), "SUMMARY": document_summary})
-    
-    
-    user_questions = [
-        "who is the insured?",
-        "What is the Insurer address?",
-        "What is the Policy Number?",
-        "Where is this event took place?",
-        "when did this event took place?",
-    ]
-    
-    user_question_chain = user_questions_prompt | llm
-    
-    answered_user_questions = []
-    for questions in user_questions:
-        temp_answer = user_question_chain.invoke({"JSONOCR": str(ocr_json), "SUMMARY": document_summary, "QUESTION":questions})
-        answered_user_questions.append(temp_answer)
-        
-    x = 0
