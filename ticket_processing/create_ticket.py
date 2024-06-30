99427a7a2ed99346f187e073db302257b81496b7
Asaad Almutareb <asaad@mutareb.com>
2024-06-30 19:15:15 +0000

    added hubspot auth token to env

--- ticket_processing/create_ticket.py
+++ ticket_processing/create_ticket.py
@@ -1,4 +1,10 @@
 import requests
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+auth_token = os.getenv('HUBSPOT_AUTH_TOKEN')
 
 class BearerAuth(requests.auth.AuthBase):
     def __init__(self, token):
@@ -15,13 +21,13 @@ tickets_endpoint = "https://api.hubapi.com/crm/v3/objects/tickets"
 #     'Content-Type': 'application/json'}
 # data = {''} 
 
-#r = requests.get(contacts_endpoint, auth=BearerAuth(auth_token))
+r = requests.get(contacts_endpoint, auth=BearerAuth(auth_token))
 
 ticket_payload = {
 
 }
 
-requests.post(tickets_endpoint, json=ticket_payload)
+#requests.post(tickets_endpoint, json=ticket_payload)
 
 
 
